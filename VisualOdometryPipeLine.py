e5f4997cf58a83160c0f7a0cc687e78f98ed7cf4
ManuelWendl <manuel.r.wedndl@gmail.com>
2024-12-29 08:30:56 +0100

    VO pipeline class

---
+++ VisualOdometryPipeLine.py
@@ -0,0 +1,97 @@
+import cv2
+import numpy as np
+
+class VisualOdometryPipeLine:
+    def __init__(self,K):
+        self.sift = cv2.SIFT_create()   # Simple SIFT detector
+        self.K = K                      # Camera matrix
+        self.pts_last = None            # Last frame keypoints TODO: Look if necessary
+        self.desc_last = None           # Last frame descriptors
+        self.keys_last = None           # Last frame keypoints
+        self.feature_ratio = 0.2        # Ratio for feature matching
+        
+
+    def initial_feature_matching(self, img0, img1):
+        # Detect keypoints and compute descriptors
+        keypoints0, descriptors0 = self.sift.detectAndCompute(img0, None)
+        keypoints1, descriptors1 = self.sift.detectAndCompute(img1, None)
+
+        # Match descriptors using BFMatcher
+        bf = cv2.BFMatcher()
+        matches = bf.knnMatch(descriptors0, descriptors1,k=2)
+
+        # Apply ratio test
+        good_matches = []
+        for m,n in matches:
+            if m.distance < self.feature_ratio*n.distance:
+                good_matches.append(m)
+
+        # Extract matched keypoints
+        pts0 = np.float32([keypoints0[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        pts1 = np.float32([keypoints1[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+        # Store descriptors of second image 
+        self.desc_last = descriptors1
+        self.keys_last = keypoints1
+
+        return pts0, pts1
+    
+    def feature_matching(self, img):
+        # Detect keypoints and compute descriptors
+        keypoints, descriptors = self.sift.detectAndCompute(img, None)
+
+        # Match descriptors using BFMatcher
+        bf = cv2.BFMatcher()
+        matches = bf.knnMatch(self.desc_last, descriptors,k=2)
+
+        # Apply ratio test
+        good_matches = []
+        for m,n in matches:
+            if m.distance < self.feature_ratio*n.distance:
+                good_matches.append(m)
+
+        # Extract matched keypoints
+        pts_last = np.float32([self.keys_last[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        pts_current = np.float32([keypoints[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+        # Store descriptors of new image
+        self.desc_last = descriptors
+        self.keys_last = keypoints
+
+        return pts_last, pts_current
+
+
+    def initialization(self, img0, img1):
+        pts_last, pts_current = self.initial_feature_matching(img0, img1)
+
+        # Estimate Essential matrix:
+        E, ransac_mask = cv2.findEssentialMat(pts_last, pts_current, self.K, method=cv2.RANSAC, prob=0.999, threshold=0.1)
+
+        # Filter inliers:
+        inl_current = pts_current[ransac_mask.ravel() == 1]
+        inl_last = pts_last[ransac_mask.ravel() == 1]
+
+        # Estimate relative camera pose of new second frame
+        _, R, t,_ = cv2.recoverPose(E, inl_last, inl_current, self.K)
+
+        self.pts_last = pts_current
+
+        return R, t
+    
+    def continuous_operation(self, img):
+        pts_last, pts_current = self.feature_matching(img)
+
+        # Estimate Essential matrix:
+        E, ransac_mask = cv2.findEssentialMat(pts_last, pts_current, self.K, method=cv2.RANSAC, prob=0.999, threshold=0.1)
+
+        # Filter inliers:
+        inl_current = pts_current[ransac_mask.ravel() == 1]
+        inl_last = pts_last[ransac_mask.ravel() == 1]
+
+        # Estimate relative camera pose of new second frame
+        _, R, t,_ = cv2.recoverPose(E, inl_last, inl_current, self.K)
+
+        self.pts_last = len(inl_current)
+
+        return R, t
+    
---
+++ utils.py
@@ -0,0 +1,84 @@
+import cv2
+import os
+import numpy as np
+
+# Define data paths
+kitti_path = './data/kitti'
+malaga_path = './data/malaga'
+parking_path = './data/parking'
+
+
+def load_data_set(ds,bootstrap_frames):
+
+    # Malaga-specific
+    left_images = None
+
+    if ds == 0:
+        # need to set kitti_path to folder containing "05" and "poses"
+        assert 'kitti_path' in globals(), "kitti_path variable must be defined"
+        ground_truth = np.loadtxt(os.path.join(kitti_path, 'poses/05.txt'))
+        ground_truth = ground_truth[:, [-9, -1]]
+        last_frame = 4540
+        K = np.array([[7.188560000000e+02, 0, 6.071928000000e+02],
+                    [0, 7.188560000000e+02, 1.852157000000e+02],
+                    [0, 0, 1]])
+        print("Kitty-Dataset loaded")
+    elif ds == 1:
+        # Path containing the many files of Malaga 7.
+        assert 'malaga_path' in globals(), "malaga_path variable must be defined"
+        images = sorted(os.listdir(os.path.join(malaga_path, 
+            'malaga-urban-dataset-extract-07_rectified_800x600_Images')))
+        left_images = images[2::2]  # Take every second file starting from the third
+        last_frame = len(left_images)
+        K = np.array([[621.18428, 0, 404.0076],
+                    [0, 621.18428, 309.05989],
+                    [0, 0, 1]])
+        print("Malaga-Dataset loaded")
+
+    elif ds == 2:
+        # Path containing images, depths and all...
+        assert 'parking_path' in globals(), "parking_path variable must be defined"
+        last_frame = 598
+        K = np.array([[331.37, 0, 320],
+                    [0, 369.568, 240],
+                    [0,0,1]])
+        ground_truth = np.loadtxt(os.path.join(parking_path, 'poses.txt'))
+        ground_truth = ground_truth[:, [-9, -1]]
+        print("Parking-Dataset loaded")
+
+    else:
+        raise ValueError("Invalid dataset selection")
+    
+        # need to set bootstrap_frames
+    if ds == 0:
+        img0 = cv2.imread(os.path.join(kitti_path, '05/image_0', f'{bootstrap_frames[0]:06d}.png'), cv2.IMREAD_GRAYSCALE)
+        img1 = cv2.imread(os.path.join(kitti_path, '05/image_0', f'{bootstrap_frames[1]:06d}.png'), cv2.IMREAD_GRAYSCALE)
+        print("Kitti bootstrap-frames loaded")
+
+    elif ds == 1:
+        img0 = cv2.imread(os.path.join(malaga_path, 'malaga-urban-dataset-extract-07_rectified_800x600_Images', left_images[bootstrap_frames[0]]), cv2.IMREAD_GRAYSCALE)
+        img1 = cv2.imread(os.path.join(malaga_path, 'malaga-urban-dataset-extract-07_rectified_800x600_Images', left_images[bootstrap_frames[1]]), cv2.IMREAD_GRAYSCALE)
+        print("Malaga bootstrap-frames loaded")
+    elif ds == 2:
+        img0 = cv2.imread(os.path.join(parking_path, f'images/img_{bootstrap_frames[0]:05d}.png'), cv2.IMREAD_GRAYSCALE)
+        img1 = cv2.imread(os.path.join(parking_path, f'images/img_{bootstrap_frames[1]:05d}.png'), cv2.IMREAD_GRAYSCALE)
+        print("Parking bootstrap-frames loaded")
+    else:
+        raise ValueError("Invalid dataset selection")
+
+    return K, img0, img1,left_images
+
+
+def load_frame(ds, i,left_images):
+    image = None
+    
+    if ds == 0:
+        image = cv2.imread(os.path.join(kitti_path, '05/image_0', f'{i:06d}.png'), cv2.IMREAD_GRAYSCALE)
+    elif ds == 1:
+        image = cv2.imread(os.path.join(malaga_path, 'malaga-urban-dataset-extract-07_rectified_800x600_Images', left_images[i]), cv2.IMREAD_GRAYSCALE)
+    elif ds == 2:
+        image = cv2.imread(os.path.join(parking_path, f'images/img_{i:05d}.png'), cv2.IMREAD_GRAYSCALE)
+    else:
+        raise ValueError("Invalid dataset selection")   
+    
+    return image
